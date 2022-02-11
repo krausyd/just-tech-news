c1089389cacf6a6dcc83eb410005be386f21bdfa
krausyd <krausyd@hotmail.com>
2022-02-10 18:19:27 -0800

    (∩◕-◕ )⊃━✿✿✿✿ Add comments model and routes

---
+++ models/Comment.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    //TABLE COLUMN DEFINITIONS GO HERE
+    // define an id column
+    id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+      },
+      // define a comment text column
+      comment_text: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      //Foreign keys
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'post',
+          key: 'id'
+        }
+      }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
